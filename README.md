240860a45a8a4442adfd41099d9aea4fa44b7620
ElimuStuart <stuartelimu@gmail.com>
2019-05-16 08:34:23 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Foodies
 HTML Template for Foodies restaurant
+
+##Requirements
+HTML5
+CSS3
+JavaScript
---
+++ js/script.js
@@ -0,0 +1,8 @@
+const MENU = document.querySelector(".menu");
+
+function toggleActive(){
+    this.classList.toggle("active");
+}
+
+MENU.addEventListener("click", toggleActive);
+
